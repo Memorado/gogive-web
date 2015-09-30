e977fe3d1a200871df785561e4a23c90a9b6526b
Karolis Stasaitis <stkarolis@gmail.com>
2015-09-30 17:59:36 +0200

    Updated API to send category info

--- routes/api/center.js
+++ routes/api/center.js
@@ -2,13 +2,20 @@ var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
 var Center = mongoose.model('Center');
+var Category = mongoose.model('Category');
 
 /* GET home page. */
 router.get('/list', function(req, res, next) {
   Center.find().populate('items').exec(function(err, centers) {
-  	res.send({
-  		centers: centers
-  	});
+    var options = {
+      path: 'items.category',
+      model: 'Category'
+    };
+    Category.populate(centers, options, function (err, center) {
+    	res.send({
+    		centers: centers
+    	});
+    });
   });
 });
 
