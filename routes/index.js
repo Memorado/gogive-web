8121371fb2ca1cac1d77301ff5edb552d8315408
Karolis Stasaitis <stkarolis@gmail.com>
2015-10-01 11:09:36 +0200

    Added favicon assets

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin/landing');
+  res.render('admin/landing', { title: 'GoGive - Connecting donators and help centers' });
 });
 
 module.exports = router;
